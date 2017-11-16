19a968265d0c7240aa8ca6d639049d75c05db943
doubleO8 <wb008@hdm-stuttgart.de>
2017-11-16 17:31:19 +0100

    add versioning and packaging helpers

---
+++ beppo/defaults.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from . import __version__
+
+PACKAGE_META = {
+    "package": "enigma2-plugin-extensions-openwebif",
+    "upstream_version": __version__,
+    "epoch": 1,
+}
+
+OUTPUT_PATH = 'pages_out'
+
+PACKAGE_OUTPUT_PATH = 'pack'

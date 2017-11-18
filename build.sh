0ddeb8d6d3636b48c4f2ac16226d00f897e3e44d
doubleO8 <wb008@hdm-stuttgart.de>
2017-11-18 09:04:10 +0100

    include coshed configuration files

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ mkdir -p ./pages_out
 python ./prepare_package_contents.py
 ./opkg-utils/opkg-build -O -o 0 -g 0 -Z gzip pack/
 python ./harvest.py
+./opkg-utils/opkg-make-index ./pages_out/ | tee ./pages_out/Packages
+if [ -f cosh.json ]; then
+    coshed-watcher.py -f cosh.json -u
+fi

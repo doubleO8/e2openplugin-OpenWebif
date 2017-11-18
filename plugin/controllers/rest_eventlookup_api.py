da11f5bff14089313b1d08a0abac28b9c92f677a
doubleO8 <wb008@hdm-stuttgart.de>
2017-11-18 16:33:05 +0100

    some debug information

--- plugin/controllers/rest_eventlookup_api.py
+++ plugin/controllers/rest_eventlookup_api.py
@@ -33,19 +33,39 @@ class EventLookupApiController(RESTControllerSkeleton):
         if "sref" in request.args:
             service_reference = request.args["sref"][0]
         else:
-            service_reference = 'IBDTSERN'
+            service_reference = '1:0:19:7C:6:85:FFFF0000:0:0:0:'
 
         data = {
             "result": False,
+            "_search_flags": [
+                ("SIMILAR_BROADCASTINGS_SEARCH", self.epgcache_instance.SIMILAR_BROADCASTINGS_SEARCH),
+                ("EXAKT_TITLE_SEARCH", self.epgcache_instance.EXAKT_TITLE_SEARCH),
+                ("PARTIAL_TITLE_SEARCH", self.epgcache_instance.PARTIAL_TITLE_SEARCH),
+                ("SHORT_DESCRIPTION_SEARCH", self.epgcache_instance.SHORT_DESCRIPTION_SEARCH),
+                ("TITLE_SHORT_DESCRIPTION_SEARCH", self.epgcache_instance.TITLE_SHORT_DESCRIPTION_SEARCH),
+                ("EXTENDED_DESCRIPTION_SEARCH", self.epgcache_instance.EXTENDED_DESCRIPTION_SEARCH),
+                ("FULL_DESCRIPTION_SEARCH", self.epgcache_instance.FULL_DESCRIPTION_SEARCH),
+                ("CASE_CHECK", self.epgcache_instance.CASE_CHECK),
+                ("NO_CASE_CHECK", self.epgcache_instance.NO_CASE_CHECK),
+            ],
             "args": {
-                "flags": flags,
+                "lookup_flags": flags,
                 "service_reference": service_reference
             }
         }
+
+        #                    service_type
+        # service_reference, ?,
+        #                    0, begintime, endtime
+        #                    2, ID # lookup by ID?
+        #                    0, -1        # NOW
+        #                    1, -1        # NEXT
+        #                    0, -1, -1    # Service
         try:
             data['events'] = self.epgcache_instance.lookupEvent(
                 [flags, (service_reference, 0, 0, -1), (service_reference, 0, 1, -1)]
             )
+            data['result'] = True
         except Exception as exc:
             data['exception'] = repr(exc)
 

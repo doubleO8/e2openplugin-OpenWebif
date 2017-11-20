0d39fe60c6827a88ddd0181bf125668d5452e268
doubleO8 <wb008@hdm-stuttgart.de>
2017-11-20 18:27:52 +0100

    add docstring

--- plugin/controllers/models/model_utilities.py
+++ plugin/controllers/models/model_utilities.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 
 def mangle_epg_text(value):
+    """
+    Remove EPG data-specific utf-8 characters.
+    Replace EPG specific newline character 0xc28a with '\n'.
+
+    """
     return value.replace(
         '\xc2\x86', '').replace('\xc2\x87', '').replace('\xc2\x8a', '\n')
 

1c781e17ed49a653f5078d20958342ea7f8f0d32
doubleO8 <wb008@hdm-stuttgart.de>
2017-11-23 21:50:26 +0100

    add verbosity option

--- plugin/controllers/rest_api_controller.py
+++ plugin/controllers/rest_api_controller.py
@@ -64,6 +64,7 @@ class ApiController(resource.Resource):
             #: ajax controller instance
             self.ajax_instance = AjaxController(session, path)
 
+        self.verbose = kwargs.get("verbose", 0)
         self._resource_prefix = kwargs.get("resource_prefix", '/api')
         self._cors_header = copy.copy(CORS_DEFAULT)
         http_verbs = []
@@ -158,11 +159,14 @@ class ApiController(resource.Resource):
             data = {
                 "method": repr(func_path),
                 "result": False,
-                "request": {
+            }
+
+            if self.verbose:
+                data["request"] = {
                     "path": request.path,
                     "postpath": request.postpath,
                 }
-            }
+
             return json_response(request, data)
 
         try:

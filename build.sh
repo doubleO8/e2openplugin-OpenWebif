a9ba3c49c2800eb0e176942c9a35d73281fe647d
doubleO8 <wb008@hdm-stuttgart.de>
2018-03-14 19:33:25 +0100

    remove old .opk files

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 export PYTHONOPTIMIZE=yeah
 rm -rf ./pages_out
 mkdir -p ./pages_out
+rm *.opk
 python ./prepare_package_contents.py
 ./opkg-utils/opkg-build -O -o 0 -g 0 -Z gzip pack/
 python ./harvest.py

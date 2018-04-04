8f243e87bee2c386035cd7a5f0ee4b2ec9face25
doubleO8 <wb008@hdm-stuttgart.de>
2018-04-04 17:02:01 +0200

    dump package meta

--- harvest.py
+++ harvest.py
@@ -3,6 +3,7 @@
 """
 CLI tool: generate github pages contents.
 """
+import pprint
 import pert_belly_hack
 from pert_belly_hack.harvesting import HarvestKeitel
 
@@ -11,6 +12,7 @@ from pert_belly_hack_backend.defaults import PACKAGE_META
 if __name__ == '__main__':
     print("Pert Belly Hack {:s}: {:s}".format(
         pert_belly_hack.__version__, 'HARVEST'))
+    pprint.pprint(PACKAGE_META)
 
     HARVEY = HarvestKeitel(package_meta=PACKAGE_META)
     HARVEY.harvest()
--- prepare_package_contents.py
+++ prepare_package_contents.py
@@ -3,6 +3,7 @@
 """
 CLI tool: generate OPKG package and meta data.
 """
+import pprint
 
 import pert_belly_hack
 from pert_belly_hack.packaging import AlPackino
@@ -12,6 +13,7 @@ from pert_belly_hack_backend.defaults import PACKAGE_META
 if __name__ == '__main__':
     print("Pert Belly Hack {:s}: {:s}".format(
         pert_belly_hack.__version__, 'PREPARE'))
+    pprint.pprint(PACKAGE_META)
 
     AL = AlPackino(package_meta=PACKAGE_META)
     AL.prepare()

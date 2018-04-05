50bf85798c3abb396f6f97071e448b481e393031
doubleO8 <wb008@hdm-stuttgart.de>
2018-04-05 18:00:27 +0200

    umbrella build process

--- build.sh
+++ build.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 export PYTHONOPTIMIZE=yeah
-rm -rf ./pages_out
-mkdir -p ./pages_out
-rm *.opk
-python ./prepare_package_contents.py
-./opkg-utils/opkg-build -O -o 0 -g 0 -Z gzip pack/
-python ./harvest.py
+
+rm -rf pages_out
+mkdir pages_out
+
+cd pert_belly_hack-backend
+pbh-clean
+./build.sh
+cd ..
+
+cd pert_belly_hack-frontend-crap
+pbh-clean
+./build.sh
+cd ..
+
+cp -r pert_belly_hack-backend/pages_out/ pages_out/backend
+cp -r pert_belly_hack-frontend-crap/pages_out/ pages_out/frontend-crap
+
 ./opkg-utils/opkg-make-index ./pages_out/ | tee ./pages_out/Packages
 gzip ./pages_out/Packages
+
 if [ -f cosh.json ]; then
     coshed-watcher.py -f cosh.json -u
 fi
